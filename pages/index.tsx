845b46f0864c4787df64840e9f7f2c47aea7c889
jpi <jpi@atolcd.com>
2022-10-05 14:38:22 +0200

    chore(init): init project
    chore(init): clean the repo (#1)
    
    Co-authored-by: jpi <jpi@atolcd.com>

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 0 2rem;
+`;
+
+const Home: NextPage = () => {
+  return (
+    <Container>
+    </Container>
+  );
+};
+
+export default Home;
