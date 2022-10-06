d13556d7d87ce85ad8c7379297b877143d8a3132
Pehlow <jpi@atolcd.com>
2022-10-06 10:47:54 +0200

    feat(nav): add navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
-import type { NextPage } from "next";
+import type {NextPage} from "next";
 import styled from "styled-components";
+import Navbartop from "../components/NavbarTop/navbartop";
 
 const Container = styled.div`
   margin: 0 2rem;
 `;
 
 const Home: NextPage = () => {
-  return (
-    <Container>
-    </Container>
-  );
+    return (<>
+            <Navbartop/>
+            <Container>
+            </Container>
+        </>
+    );
 };
 
 export default Home;
