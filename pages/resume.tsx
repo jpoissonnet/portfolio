b711944f03da32e9dedcc42ef216ef21c736ae68
jpi <jpi@atolcd.com>
2022-10-23 17:33:40 +0200

    feat(resume): create resume page

---
+++ pages/resume.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import NavbarTop from "../components/Navigation/NavbarTop/navbartop";
+import Container from "../components/Layout/Container/container";
+
+const Resume = () => {
+  return (
+    <>
+      <NavbarTop />
+      <Container>
+        <h1>Resume</h1>
+        <p>
+          I am curious and I like to learn many things that is why I like computer science that much. I love the feeling of
+          getting better at something. What I also like is to make visual things. Front-end development is a good way to
+          combine both of these things. It is wide and there is always something new to learn.
+        </p>
+
+      </Container>
+
+    </>
+  );
+};
+
+export default Resume;
