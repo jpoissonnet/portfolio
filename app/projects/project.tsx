607c046c9c12bd02b5f94bbaee016f78d22203c5
Pehlow <poissonnet.jules@gmail.com>
2022-12-05 12:47:30 +0100

    border

--- app/projects/project.tsx
+++ app/projects/project.tsx
@@ -5,13 +5,18 @@ import Image from "next/image";
 const Project = ({ project }: any) => {
   return (
     <Link
-      href={"/"}
+      href={project.slug}
       className={
-        "flex basis-1/4 flex-col justify-between " +
-        "items-start gap-1 h-[350px] m-8"
+        "flex basis-[250px] flex-col justify-between items-start gap-1 h-[350px] m-4 p-2 " +
+        "border border-transparent hover:border-slate-50 rounded transition-all duration-300 ease-in-out "
       }
     >
-      <div className={"flex-shrink-0 flex-grow-0 basis-2/3 w-full relative"}>
+      <div
+        className={
+          "flex-shrink-0 flex-grow-0 basis-1/2 w-full relative " +
+          "transition-all duration-300"
+        }
+      >
         <Image
           src={project.coverImage}
           alt={project.alt ?? "decorative image"}
