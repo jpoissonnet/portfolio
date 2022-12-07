c3c68e2563e7a9cb96fb750861ad3cc17c112937
Pehlow <poissonnet.jules@gmail.com>
2022-12-07 14:45:41 +0100

    fix zindex

--- app/(layout)/navigation/navbar.tsx
+++ app/(layout)/navigation/navbar.tsx
@@ -4,7 +4,7 @@ import { NavMobile } from "./mobilenav";
 
 const NavbarTop = () => {
   return (
-    <nav className={"flex justify-between items-center h-16 bg-slate-800"}>
+    <nav className={"relative z-10 flex justify-between items-center h-16 bg-slate-800"}>
       <div
         className={
           "flex justify-between items-center w-full max-w-screen-lg mx-auto px-4"
