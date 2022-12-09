1a8e91ee2d6bb400920943228602e931de4957c0
Pehlow <poissonnet.jules@gmail.com>
2022-12-09 11:09:44 +0100

    pills without animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,18 @@ module.exports = {
   theme: {
     extend: {}
   },
+  safelist: [
+    "bg-red-800",
+    "bg-green-800",
+    "bg-blue-800",
+    "bg-amber-800",
+    "bg-indigo-800",
+    "bg-purple-800",
+    "bg-orange-800",
+    "bg-pink-800",
+    "bg-gray-800",
+    "bg-cyan-800",
+    "bg-slate-800"
+  ],
   plugins: []
 };
