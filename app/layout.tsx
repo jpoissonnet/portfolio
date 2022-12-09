d47bdb1deb15a453fca780f667cb4465406b5bdb
Pehlow <poissonnet.jules@gmail.com>
2022-12-09 19:31:34 +0100

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,23 @@
 import "./globals.css";
 import NavbarTop from "./(layout)/navigation/navbar";
 import { ReactNode } from "react";
+import Footer from "./(layout)/footer";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="fr">
-    {/*
+      {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-    <head />
-    <body>
-    <NavbarTop />
-    <main className={"relative z-0 max-w-screen-lg m-auto px-4"}>{children}</main>
-    </body>
+      <head />
+      <body>
+        <NavbarTop />
+        <main className={"relative z-0 max-w-screen-lg m-auto px-4"}>
+          {children}
+        </main>
+        <Footer />
+      </body>
     </html>
   );
 }
