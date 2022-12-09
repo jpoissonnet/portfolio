01a1e6aaf19a1e953cb5b72f4bbffae7ab34ebfd
Pehlow <poissonnet.jules@gmail.com>
2022-12-09 17:13:15 +0100

    working slug page

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { getProjectBySlug } from "../projectsConnector";
 import Image from "next/image";
+import Content from "./content";
+import { serialize } from "next-mdx-remote/serialize";
 
-const Page = ({ params }: { params: any }) => {
-  const project = getProjectBySlug(params.slug, ["title", "content", "coverImage"]);
+const Page = async ({ params }: { params: any }) => {
+  const project = getProjectBySlug(params.slug, [
+    "title",
+    "content",
+    "coverImage",
+  ]);
   return (
     <>
-      <h1>{project.title}</h1>
-      <div className={"relative"}>
+      <h1 className={"text-5xl mb-5"}>{project.title}</h1>
+      <div className={"relative h-32 md:h-64 w-full my-8 md:my-4"}>
         <Image
           src={project.coverImage}
           alt={project.title}
           fill={true}
-          style={{ objectFit: "cover", objectPosition: "center" }}
+          style={{ objectFit: "cover", objectPosition: "bottom" }}
         />
       </div>
-      <div dangerouslySetInnerHTML={{ __html: project.content }} />
+      <Content content={await serialize(project.content)} />
     </>
   );
 };
