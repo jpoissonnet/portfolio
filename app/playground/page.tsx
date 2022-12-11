d6639b97797b86f5edcbe8a0450382681e354f9f
Pehlow <poissonnet.jules@gmail.com>
2022-12-11 13:27:07 +0100

    playground page

---
+++ app/playground/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Playground = () => {
+  return (
+    <section>
+      <h1 className={"text-2xl font-bold my-5"}>Welcome to the playground!</h1>
+      <p className={"text-gray-500"}>
+        This is the page where I will put all my experiments, tests and other
+        stuff.
+      </p>
+    </section>
+  );
+};
+
+export default Playground;
