06549d17bdf5468fbd43bd687d6613a661266761
Pehlow <poissonnet.jules@gmail.com>
2023-09-28 22:38:34 +0200

    add skeleton and prettier

--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -1,19 +1,25 @@
 <script setup>
 const route = useRoute();
-const project = useAsyncData("projects", () => queryContent("projects").where({ _path: route.path }).findOne()).data;
-
+const project = useAsyncData("projects", () =>
+  queryContent("projects").where({ _path: route.path }).findOne(),
+).data;
 </script>
 
 <template>
-  <div><h2>{{ project.title }}</h2>
-    <section>
-      <img
-        :src="project.coverImage"
-        :alt="project.title"
-      />
-      <ContentRenderer tag="article" :value="project" />
-    </section>
-  </div>
+  <h2>{{ project.title }}</h2>
+  <section>
+    <img :src="project.coverImage" :alt="project.title" />
+    <ContentRenderer tag="article" :value="project">
+      <template #empty>
+        <div class="skeleton-title"></div>
+        <div class="skeleton-text"></div>
+        <div class="skeleton-title"></div>
+        <div class="skeleton-text"></div>
+        <div class="skeleton-title"></div>
+        <div class="skeleton-text"></div>
+      </template>
+    </ContentRenderer>
+  </section>
 </template>
 
 <style scoped>
@@ -39,4 +45,58 @@ img {
   object-fit: cover;
 }
 
+.skeleton-title {
+  width: 16ch;
+  height: 1em;
+  font-size: 2em;
+  margin: 0.67em 0;
+  background: #383838;
+  border-radius: 1em;
+  position: relative;
+  overflow: hidden;
+
+  &::after {
+    content: "";
+    position: absolute;
+    inset: 0;
+    background-image: linear-gradient(
+      90deg,
+      rgba(255, 255, 255, 0) 0,
+      rgba(255, 255, 255, 0.5) 50%,
+      rgba(255, 255, 255, 0) 100%
+    );
+    animation: shimmer 1s infinite;
+  }
+}
+
+.skeleton-text {
+  width: 100%;
+  height: 4em;
+  background: #383838;
+  border-radius: 1em;
+  position: relative;
+  overflow: hidden;
+
+  &::after {
+    content: "";
+    position: absolute;
+    inset: 0;
+    background-image: linear-gradient(
+      90deg,
+      rgba(255, 255, 255, 0) 0,
+      rgba(255, 255, 255, 0.5) 50%,
+      rgba(255, 255, 255, 0) 100%
+    );
+    animation: shimmer 1s ease-in-out infinite;
+  }
+}
+
+@keyframes shimmer {
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(100%);
+  }
+}
 </style>
